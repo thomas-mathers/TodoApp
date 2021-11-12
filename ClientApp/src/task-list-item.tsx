fbbfda5105c83ae9aa5bb5c96768bb692e6f2cb5
Thomas Mathers <thomas.mathers@ideanotion.net>
2021-11-12 16:11:16 -0500

    disable task list item mount animation

--- ClientApp/src/task-list-item.tsx
+++ ClientApp/src/task-list-item.tsx
@@ -1,5 +1,5 @@
 import { Cancel, CheckCircle, Circle, Delete, Done, Edit } from "@mui/icons-material";
-import { Checkbox, IconButton, Box, Stack, TextField, Tooltip, Typography, Grow } from "@mui/material";
+import { Checkbox, IconButton, Box, Stack, TextField, Tooltip, Typography } from "@mui/material";
 import { ChangeEvent, Fragment, useState } from "react";
 import { useDialog } from "./hooks/use-dialog";
 import { TaskListItemProps } from "./task-list-item-props";
@@ -48,52 +48,50 @@ const TaskListItem = (props: TaskListItemProps) => {
 
     return (
         <div style={props.style}>
-            <Grow in={true}>
-                <Stack direction="row" alignItems="center" spacing={1} padding={2}>
-                    <Checkbox edge="start" checked={task.dateCompleted !== null} onChange={handleCompleteToggle} icon={<Circle />} checkedIcon={<CheckCircle />} />
-                    <Box flex="1" overflow="hidden">
+            <Stack direction="row" alignItems="center" spacing={1} padding={2}>
+                <Checkbox edge="start" checked={task.dateCompleted !== null} onChange={handleCompleteToggle} icon={<Circle />} checkedIcon={<CheckCircle />} />
+                <Box flex="1" overflow="hidden">
+                    {
+                        task.editing ?
+                            <TextField variant="standard" value={description} onChange={handleChangeDescription} hiddenLabel fullWidth />
+                            :
+                            <Typography variant="body1" className={task.dateCompleted !== null ? 'task-description complete' : 'task-description'}>{task.description}</Typography>
+                    }
+                </Box>
+                {
+                    task.dateCompleted == null &&
+                    <Stack direction="row">
                         {
                             task.editing ?
-                                <TextField variant="standard" value={description} onChange={handleChangeDescription} hiddenLabel fullWidth />
+                                <Fragment>
+                                    <Tooltip title="Done">
+                                        <IconButton onClick={handleConfirmEdit}>
+                                            <Done />
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Tooltip title="Cancel">
+                                        <IconButton onClick={handleDiscardEdit}>
+                                            <Cancel />
+                                        </IconButton>
+                                    </Tooltip>
+                                </Fragment>
                                 :
-                                <Typography variant="body1" className={task.dateCompleted !== null ? 'task-description complete' : 'task-description'}>{task.description}</Typography>
+                                <Fragment>
+                                    <Tooltip title="Edit">
+                                        <IconButton onClick={handleEdit}>
+                                            <Edit />
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Tooltip title="Delete">
+                                        <IconButton onClick={handleDelete}>
+                                            <Delete />
+                                        </IconButton>
+                                    </Tooltip>
+                                </Fragment>
                         }
-                    </Box>
-                    {
-                        task.dateCompleted == null &&
-                        <Stack direction="row">
-                            {
-                                task.editing ?
-                                    <Fragment>
-                                        <Tooltip title="Done">
-                                            <IconButton onClick={handleConfirmEdit}>
-                                                <Done />
-                                            </IconButton>
-                                        </Tooltip>
-                                        <Tooltip title="Cancel">
-                                            <IconButton onClick={handleDiscardEdit}>
-                                                <Cancel />
-                                            </IconButton>
-                                        </Tooltip>
-                                    </Fragment>
-                                    :
-                                    <Fragment>
-                                        <Tooltip title="Edit">
-                                            <IconButton onClick={handleEdit}>
-                                                <Edit />
-                                            </IconButton>
-                                        </Tooltip>
-                                        <Tooltip title="Delete">
-                                            <IconButton onClick={handleDelete}>
-                                                <Delete />
-                                            </IconButton>
-                                        </Tooltip>
-                                    </Fragment>
-                            }
-                        </Stack>
-                    }
-                </Stack>
-            </Grow>
+                    </Stack>
+                }
+            </Stack>
         </div>
     );
 };
